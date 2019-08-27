c3b1e5e5a265ba7c31850c06d6f8566c6c83687d
Hugh Hoang <hugh.hoang@gmail.com>
2019-08-27 23:40:00 +1200

    feat: fetch *.kdbx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useAuthContext } from '../contexts'
+import { ListItem, ListItemText, List } from '@material-ui/core'
 
 export default () => {
   const { accessToken } = useAuthContext()
@@ -10,8 +11,9 @@ export default () => {
       return
     }
     const options = {
-      pageSize: 10,
-      fields: 'files(id, name, webContentLink)'
+      pageSize: 100,
+      fields: 'files(id, name, webContentLink)',
+      q: `name contains '.kdbx'`
     }
     const fetchFiles = async () => {
       const response = await window.gapi.client.drive.files.list(options)
@@ -20,11 +22,20 @@ export default () => {
     fetchFiles()
   }, [accessToken])
 
-  console.log(files)
+  const download = link => {
+    console.log(link)
+  }
 
   return (
     <>
       <h1>Home</h1>
+      <List>
+        {files.map(({ id, name, webContentLink }) => (
+          <ListItem button key={id} onClick={() => download(webContentLink)}>
+            <ListItemText primary={name} />
+          </ListItem>
+        ))}
+      </List>
     </>
   )
 }
