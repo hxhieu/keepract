acf427b50e9f15e3d4dbf87e1a06bd2f4f907d1b
Hugh Hoang <hugh.hoang@gmail.com>
2019-09-25 22:31:36 +1200

    feat: group in popup for reusability

--- src/components/ProjectDatabase.tsx
+++ src/components/ProjectDatabase.tsx
@@ -1,52 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import {
-  Dialog,
-  Container,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography
-} from '@material-ui/core'
-import CloseIcon from '@material-ui/icons/Close'
 import * as str2ab from 'string-to-arraybuffer'
 import { IProject } from '../types'
 import { Credentials, Kdbx, Group } from 'kdbxweb'
-import KdbxGroup from '../components/kdbx/KdbxGroup'
-import ScreenLoader from './common/ScreenLoader'
+import KdbxGroupPopup from './kdbx/KdbxGroupPopup'
 
 interface IGroupList {
   list?: Group[]
   loading: boolean
 }
 
-const useStyles = makeStyles(theme => ({
-  appBar: {
-    position: 'relative'
-  },
-  title: {
-    marginLeft: theme.spacing(2),
-    flex: 1
-  }
-}))
-
 export default ({
   project,
-  open,
   onClose
 }: {
-  project?: IProject
-  open: boolean
+  project: IProject
   onClose: () => void
 }) => {
-  const classes = useStyles()
   const [groups, setGroups] = useState<IGroupList>({
     loading: true,
     list: undefined
   })
 
   useEffect(() => {
-    if (!project) return
     const { kdbxFileId: fileId } = project
     if (!fileId) {
       alert('Corrupted project, database undefined')
@@ -77,29 +52,11 @@ export default ({
     fetchFile()
   }, [project])
 
-  const { loading, list } = groups
-
   return (
-    <Dialog fullScreen open={open}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            {project && project.name}
-          </Typography>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="close"
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="md">
-        <ScreenLoader loading={loading} />
-        {list && list.length && <KdbxGroup group={list[0]} />}
-      </Container>
-    </Dialog>
+    <KdbxGroupPopup
+      open={!!project}
+      group={groups.list && groups.list[0]}
+      onClose={onClose}
+    />
   )
 }
