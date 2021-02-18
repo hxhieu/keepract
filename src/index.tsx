5de421e26dae30001aae4f708fd72f9dac49d821
Hugh Hoang <hugh.hoang@gmail.com>
2021-02-18 13:51:33 +1300

    chore: latest service worker and manifest

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import * as Sentry from '@sentry/browser'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 // Sentry.init({
 //   dsn: 'https://c2cd2cc2d5564941a2bddc86a9da9b60@sentry.io/1759677',
@@ -14,8 +13,3 @@ gapi.load('client', async () => {
   await gapi.client.load('drive', 'v3')
   ReactDOM.render(<App />, document.getElementById('root'))
 })
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import styleImport from 'vite-plugin-style-import'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,33 @@ export default defineConfig({
     },
   },
   plugins: [
+    VitePWA({
+      manifest: {
+        short_name: 'Mitmeo Vault',
+        name: 'Mitmeo Vault',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+    }),
     reactRefresh(),
     styleImport({
       libs: [
